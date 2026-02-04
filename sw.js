3f29c80e113690dbf77a3283309c4105d54c4f00
rzlotolow <rzlotolow123@gmail.com>
2026-02-04 16:41:04 -0600

    Refactor service worker event listeners

--- sw.js
+++ sw.js
@@ -1,15 +1,12 @@
+const CACHE = 'lifting-companion-v1';
+const FILES = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
-+     1: const CACHE = 'lifting-companion-v1';
-+     2: const FILES = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
-+     3: 
-+     4: self.addEventListener('install', e => {
-+     5:     e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
-+     6: });
-+     7: 
-+     8: self.addEventListener('fetch', e => {
-+     9:     e.respondWith(
-+    10:         caches.match(e.request).then(response => response || fetch(e.request))
-+    11:     );
-+    12: });
+self.addEventListener('install', e => {
+   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
 
-Creating: /workspace/lifting-companion/sw.js
+self.addEventListener('fetch', e => {
+   e.respondWith(
+       caches.match(e.request).then(response => response || fetch(e.request))
+   );
+});
