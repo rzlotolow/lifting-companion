b3a45b03c8ec048ad0ccf51ec3c327aa6e40c0be
rzlotolow <rzlotolow123@gmail.com>
2026-02-04 16:56:27 -0600

    Update cached files path in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 const CACHE = 'lifting-companion-v1';
-const FILES = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
+const FILES = [
+   '/lifting-companion/',
+   '/lifting-companion/index.html',
+   '/lifting-companion/style.css',
+   '/lifting-companion/app.js',
+   '/lifting-companion/manifest.json'
+];
 
 self.addEventListener('install', e => {
    e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
